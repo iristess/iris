87da914348a39e06da7986b89b54d02b5552dafa
iristess <iristeslerr@gmail.com>
2026-06-20 00:02:07 +0300

    Update service worker cache and app shell files
    Updated cache name and added new files to the app shell.

--- neuro-quiz-github-pages/service-worker.js
+++ neuro-quiz-github-pages/service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'neuro-quiz-v1';
+const CACHE_NAME = 'neuro-quiz-v3';
 const APP_SHELL = [
   './',
   './index.html',
-  './neuro_quiz.html',
+  './questions-new.js',
   './manifest.json',
-  './icons/icon.svg'
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
