1bef718abddd7d225d4c66dcd22516a5bed261e0
Iris Tesler <iristess@users.noreply.github.com>
2026-07-04 15:45:20 +0300

    Set Nat question to feminine wording

--- neuro-quiz-github-pages/service-worker.js
+++ neuro-quiz-github-pages/service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'neuro-quiz-v18';
+const CACHE_NAME = 'neuro-quiz-v19';
 const APP_SHELL = [
   './',
   './index.html',
-  './questions-new.js?v=clinical-names-v18',
+  './questions-new.js?v=clinical-names-v19',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
