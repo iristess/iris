c019c5f2d045e80453bc45fd084da1d00d8c2d01
Iris Tesler <iristess@users.noreply.github.com>
2026-07-04 15:42:50 +0300

    Update Nat and Shai clinical names

--- neuro-quiz-github-pages/service-worker.js
+++ neuro-quiz-github-pages/service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'neuro-quiz-v17';
+const CACHE_NAME = 'neuro-quiz-v18';
 const APP_SHELL = [
   './',
   './index.html',
-  './questions-new.js?v=clinical-names-v17',
+  './questions-new.js?v=clinical-names-v18',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
