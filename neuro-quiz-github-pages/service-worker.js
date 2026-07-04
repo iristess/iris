0635f30bfe06434b0172e5731952858a573e5b4d
Iris Tesler <iristess@users.noreply.github.com>
2026-07-04 15:38:59 +0300

    Add Rita clinical integration question

--- neuro-quiz-github-pages/service-worker.js
+++ neuro-quiz-github-pages/service-worker.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'neuro-quiz-v16';
+const CACHE_NAME = 'neuro-quiz-v17';
 const APP_SHELL = [
   './',
   './index.html',
-  './questions-new.js?v=clinical-names-v16',
+  './questions-new.js?v=clinical-names-v17',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png'
